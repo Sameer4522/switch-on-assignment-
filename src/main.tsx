cd355254e14edb7ab825f3e8e806a13535378fea
Sameer Jain <sameer.jain@synlabs.io>
2026-09-20 21:46:35 +0530

    provider setup

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,18 @@
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
-import { App } from './App';
-import './styles.css';
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { App } from "./App";
+import "./styles.css";
 
-const container = document.getElementById('root');
-if (!container) throw new Error('Missing #root');
+const container = document.getElementById("root");
+if (!container) throw new Error("Missing #root");
+
+const queryClient = new QueryClient();
 
 createRoot(container).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+	<StrictMode>
+		<QueryClientProvider client={queryClient}>
+			<App />
+		</QueryClientProvider>
+	</StrictMode>
 );
